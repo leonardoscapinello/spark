aa1aaffe6d33641d6108819481565ed5e0b2ca84
appstwobrain <152556663+appstwobrain@users.noreply.github.com>
2026-09-12 06:31:26 -0300

    Use Atendimento vocabulary in navigation stories

--- packages/ui-web/src/Sidebar/Sidebar.stories.tsx
+++ packages/ui-web/src/Sidebar/Sidebar.stories.tsx
@@ -4,4 +4,4 @@ import { SidebarItem } from "./Sidebar.js";
 const meta: Meta<typeof Sidebar> = { title: "Componentes/Sidebar", component: Sidebar };
 export default meta;
 type Story = StoryObj<typeof Sidebar>;
-export const Default: Story = { render: () => (<Sidebar title="Inbox"><SidebarItem href="#all" active count={4}>Todos</SidebarItem><SidebarItem href="#mine" count={0}>Sua caixa de entrada</SidebarItem></Sidebar>) };
+export const Default: Story = { render: () => (<Sidebar title="Atendimento"><SidebarItem href="#all" active count={4}>Todas</SidebarItem><SidebarItem href="#mine" count={0}>Minhas conversas</SidebarItem></Sidebar>) };
